7d0f0c72977fa501e23718aaee2c269a2ac3325e
Karapuchlo <123234471+Karapuchlo@users.noreply.github.com>
2023-06-07 15:10:05 +0300

    исправил ошибки

--- create_tables.sql
+++ create_tables.sql
@@ -5,18 +5,18 @@ CREATE TABLE employees
 	title varchar(50),
 	birth_date DATE,
 	notes TEXT
-)
+);
 CREATE TABLE customer
 (
 	customer_id	varchar(10) UNIQUE,
 	company_name varchar(100),	
 	contact_name varchar(100)
-)
+);
 CREATE TABLE orders
 (
-	order_id int PRYMARY_KEY,
+	order_id int PRYMARY KEY,
 	customer_id	varchar(50),
 	employee_id	int,
 	order_date	DATE,
 	ship_city varchar(50)
-)
+);
